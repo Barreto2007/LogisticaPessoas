5fa6ed41e344860a88c9972760bfd2f204f5f3b1
Cleber <Cleber@DESENVWEB>
2016-05-29 18:26:08 -0300

    Add project files.

---
+++ LogisticaPessoasTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogisticaPessoasTest
+{
+    [TestClass]
+    public class LogisticaPessoas
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
